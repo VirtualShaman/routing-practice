3b576062bdcc556b3f928b69e9d22c4331a3b886
Tyler Knight <TylerKnight2197@gmail.com>
2021-11-03 17:50:32 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { useParams } from "react-router";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link 
+} from "react-router-dom";
+    
+const Home = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Welcome</h1>
   );
 }
 
+const Input = (props) => {
+  const { input } = useParams();
+  if(isNaN(input)==false){
+    return (
+      <h1>The number is: { input }</h1>
+    )
+  } else {
+    return (
+      <h1>The word is: { input }</h1>
+    )
+  }
+}
+
+const Style =  (props) => {
+  const { input, color1, color2 }= useParams();
+  if(isNaN(input)==false){
+    return (
+      <h1 style={{color: color1, backgroundColor: color2}}>The number is: { input }</h1>
+    )
+  } else {
+    return (
+      <h1 style={{color: color1, backgroundColor: color2}}>The word is: { input }</h1>
+    )
+  }
+}
+    
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+        <Route exact path="/:input">
+          <Input />
+        </Route>
+        <Route exact path="/:input/:color1/:color2">
+          <Style />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+    
 export default App;
